0cbdf55aa7b1175e4b3ae825062e157b31eb78d1
auzaniman <auzan.ip@gmail.com>
2021-04-26 09:17:21 +0700

    update sign in to sign out on dashboard

--- resources/views/user_officer/pages/dashboard.blade.php
+++ resources/views/user_officer/pages/dashboard.blade.php
@@ -122,7 +122,7 @@ Dashboard
         </a>
         </li>
         <li class="nav-item">
-        <a class="nav-link  " href="../pages/sign-in.html">
+        <a class="nav-link  " href="{{route('auth.logout_officer')}}">
             <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
             <svg width="12px" height="12px" viewBox="0 0 40 44" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                 <title>document</title>
@@ -138,7 +138,7 @@ Dashboard
                 </g>
             </svg>
             </div>
-            <span class="nav-link-text ms-1">Sign In</span>
+            <span class="nav-link-text ms-1">Sign Out</span>
         </a>
         </li>
         <li class="nav-item">
@@ -200,9 +200,9 @@ Dashboard
         </div>
         <ul class="navbar-nav  justify-content-end">
             <li class="nav-item d-flex align-items-center">
-            <a href="javascript:;" class="nav-link text-body font-weight-bold px-0">
+            <a href="{{route('auth.logout_officer')}}" class="nav-link text-body font-weight-bold px-0">
                 <i class="fa fa-user me-sm-1"></i>
-                <span class="d-sm-inline d-none">Sign In</span>
+                <span class="d-sm-inline d-none">Sign Out</span>
             </a>
             </li>
             <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
