c7dc2f172ad4f1035c9424827198dcf1d09fa5f7
M Zufar Taqiuddin <mrrafuzz.co@gmail.com>
2021-04-28 22:46:01 +0700

    Link User Member on Dashboard Officer

--- resources/views/user_officer/includes/aside.blade.php
+++ resources/views/user_officer/includes/aside.blade.php
@@ -11,7 +11,11 @@
     <div class="collapse navbar-collapse  w-auto" id="sidenav-collapse-main">
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link  active" href="#">
+                <a class="nav-link
+                    @if (\Request::is('user_officer/dashboard*'))
+                    active
+                    @endif"
+                   href="{{ route('user_officer.dashboard') }}">
                     <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                         <svg width="12px" height="12px" viewBox="0 0 45 40" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                             <title>shop </title>
@@ -31,7 +35,11 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link  " href="../pages/tables.html">
+                <a class="nav-link
+                    @if (\Request::is('user_officer/user_member*'))
+                    active
+                    @endif"
+                    href="{{ route('user_officer.user_member.index') }}">
                     <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                     <svg width="12px" height="12px" viewBox="0 0 42 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                         <title>office</title>
