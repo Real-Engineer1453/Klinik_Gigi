60624be2e29c397b3c67db52e585b74503f4871c
auzaniman <auzan.ip@gmail.com>
2021-04-28 09:50:02 +0700

    update login officer

--- resources/views/user_officer/auth/login.blade.php
+++ resources/views/user_officer/auth/login.blade.php
@@ -20,55 +20,31 @@ Login
         <!-- Navbar -->
         <nav class="navbar navbar-expand-lg  blur blur-rounded top-0  z-index-3 shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
             <div class="container-fluid">
-            <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 " href="#">
-                Soft UI Dashboard
-            </a>
-            <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon mt-2">
-                <span class="navbar-toggler-bar bar1"></span>
-                <span class="navbar-toggler-bar bar2"></span>
-                <span class="navbar-toggler-bar bar3"></span>
-                </span>
-            </button>
-            <div class="collapse navbar-collapse" id="navigation">
-                <ul class="navbar-nav mx-auto">
-                <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center me-2 active" aria-current="page" href=".#">
-                    <i class="fa fa-chart-pie opacity-6 text-dark me-1"></i>
-                    Dashboard
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link me-2" href="#">
-                    <i class="fa fa-user opacity-6 text-dark me-1"></i>
-                    Profile
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link me-2" href="#">
-                    <i class="fas fa-user-circle opacity-6 text-dark me-1"></i>
-                    Sign Up
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link me-2" href="{{route('login_officer')}}">
-                    <i class="fas fa-key opacity-6 text-dark me-1"></i>
-                    Sign In
-                    </a>
-                </li>
-                </ul>
-                <ul class="navbar-nav d-lg-block d-none">
-                <li class="nav-item">
-                    <a href="https://www.creative-tim.com/product/soft-ui-dashboard" class="btn btn-sm btn-round mb-0 me-1 bg-gradient-dark">Free download</a>
-                </li>
-                </ul>
-            </div>
+                <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon mt-2">
+                    <span class="navbar-toggler-bar bar1"></span>
+                    <span class="navbar-toggler-bar bar2"></span>
+                    <span class="navbar-toggler-bar bar3"></span>
+                    </span>
+                </button>
+                <div class="collapse navbar-collapse" id="navigation">
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item">
+                            <a class="nav-link font-weight-bolder d-flex align-items-center me-2 active" aria-current="page" href=".#">
+                            <i class="fa fa-chart-pie opacity-6 text-dark me-1"></i>
+                            ADMIN PKS MUDA
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
         <!-- End Navbar -->
         </div>
     </div>
 </div>
+
+{{-- Footer Login --}}
 <section>
 <div class="page-header section-height-75">
     <div class="container">
@@ -127,33 +103,4 @@ Login
         </div>
     </div>
 </div>
-</section>
-<!-- -------- START FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
-<footer class="footer py-4 fixed-bottom">
-<div class="container">
-    <div class="row">
-    <div class="col-lg-8 mb-4 mx-auto text-center">
-        <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-        Company
-        </a>
-        <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-        About Us
-        </a>
-        <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-        Team
-        </a>
-        <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-        Products
-        </a>
-        <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-        Blog
-        </a>
-        <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-        Pricing
-        </a>
-    </div>
-    </div>
-</div>
-</footer>
 @endsection
-<!-- -------- END FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
