1c0e923afa3cafdde1208f0ddd0f024e0d99a659
M Zufar Taqiuddin <mrrafuzz.co@gmail.com>
2021-06-04 20:53:13 +0700

    Fix Modal anamnesa

--- resources/views/user_officer/pages/user_member/show.blade.php
+++ resources/views/user_officer/pages/user_member/show.blade.php
@@ -49,40 +49,46 @@ Show Rekam Medis
                 <tbody>
 
                 @foreach($data as $key => $d)
+
+                  <!-- Modal -->
+                  <div class="modal fade" id="modal{{ $d->id }}" tabindex="-1" role="dialog" aria-labelledby="modal-default" aria-hidden="true">
+                    <div class="modal-dialog modal- modal-dialog-centered modal-" role="document">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h6 class="modal-title" id="modal-title-default">Type your modal title</h6>
+                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                          </button>
+                        </div>
+                        <div class="modal-body">
+                          <div class="row">
+                            <div class="col">
+                              <p>{{ $d->anamnesa }}</p>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn bg-gradient-primary">Save changes</button>
+                          <button type="button" class="btn btn-link  ml-auto" data-bs-dismiss="modal">Close</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
                   <tr>
                     <td class="align-middle text-center text-sm">
                       <span class="text-secondary text-sm font-weight-bold">{{ ($data->currentpage()-1) * $data->perpage() + $key + 1 }}</span>
                     </td>
                     <td class="align-middle text-sm" style="word-wrap: break-word">
-                      <span class="text-secondary text-sm font-weight-bold">{{ Str::limit($d->anamnesa, 20) }}</span>
-                      <button type="button" class="btn btn-sm bg-gradient-primary" data-bs-toggle="modal" 
-                        data-bs-target="#modal{{ $d->id }}">
-                        Launch demo modal
-                      </button>
 
-                      <div class="modal fade" id="modal{{ $d->id }}" tabindex="-1" role="dialog" aria-labelledby="modal-default" aria-hidden="true">
-                      <div class="modal-dialog modal- modal-dialog-centered modal-" role="document">
-                        <div class="modal-content">
-                          <div class="modal-header">
-                            <h6 class="modal-title" id="modal-title-default">Type your modal title</h6>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-                              <span aria-hidden="true">×</span>
-                            </button>
-                          </div>
-                          <div class="modal-body">
-                            <div class="row">
-                              <div class="col">
-                                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-                                <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
-                              </div>
-                            </div>
-                          </div>
-                          <div class="modal-footer">
-                            <button type="button" class="btn bg-gradient-primary">Save changes</button>
-                            <button type="button" class="btn btn-link  ml-auto" data-bs-dismiss="modal">Close</button>
-                          </div>
-                        </div>
-                      </div>
+                      <!-- Button trigger modal -->
+                      <button type="button" class="btn btn-sm bg-gradient-primary" data-bs-toggle="modal"
+                              data-bs-target="#modal{{ $d->id }}">
+                        Detail
+                      </button>
+                      &nbsp;
+                      <span class="text-secondary text-sm font-weight-bold">{{ Str::limit($d->anamnesa, 20) }}</span>
+
                     </td>
                     <td class="align-middle text-sm">
                       <span class="text-secondary text-sm font-weight-bold">{{ $d->riwayat_alergi }}</span>
